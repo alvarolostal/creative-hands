215f7cd38f4d2c967c35d4b4c5556d030074ed80
alvarolostal <alvarolostal04@gmail.com>
2025-11-08 17:28:43 +0100

    fix: Cambiar almacenamiento de token de localStorage a sessionStorage en interceptor de axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -12,7 +12,7 @@ const api = axios.create({
 // Interceptor para añadir el token JWT a todas las peticiones
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
